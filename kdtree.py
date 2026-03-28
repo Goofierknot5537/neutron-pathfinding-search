8e5a027d86fc2b96ecd700185267cb7b81f8215e
Goofierknot <62623578+Goofierknot5537@users.noreply.github.com>
2026-03-28 00:15:45 -0400

    Basic algorithm (slow)

--- kdtree.py
+++ kdtree.py
@@ -3,14 +3,14 @@ import math
 import datetime
 
 class Node:
-    def __init__(self, data: tuple[float, float, float], id64: int):
+    def __init__(self, pos: tuple[float, float, float], id64: int):
         self.left = None
         self.right = None
-        self.data = data
+        self.pos = pos
         self.id64 = id64
 
 def make_tree():
-    with jsonlines.open("neutrons_stripped.jsonl", mode='r') as reader:
+    with jsonlines.open("neutrons_test.jsonl", mode='r') as reader:
         first = reader.read()
         coords = first["crds"]
         root = Node((coords['x'], coords['y'], coords['z']), first["id"])
@@ -25,25 +25,11 @@ def make_tree():
 
 def test():
     root = make_tree()
-    nodes = radius_search(root, (-4942.9375, -2128.84375, 18043.6875), 100, 0)
-    for node in nodes:
-        print(node.id64)
-
-# https://opendsa-server.cs.vt.edu/ODSA/Books/CS3/html/KDtree.html Example 15.5.3
-def radius_search(kd_root: Node, point: tuple[float, float, float], dist: float, depth: int) -> list[Node]:
-    if kd_root == None:
-        return []
-    node_list = []
-    axis = depth % 3 # 0 = x, 1 = y, 2 = z
-    data = kd_root.data
-    if node_in_radius(kd_root, point, dist):
-        node_list.append(kd_root)
-    if data[axis] >= (point[axis] - dist):
-        node_list.extend(radius_search(kd_root.left, point, dist, depth + 1))
-    if data[axis] < (point[axis] + dist):
-        node_list.extend(radius_search(kd_root.right, point, dist, depth + 1))
-
-    return node_list
+    #nodes = radius_search(root, (-4942.9375, -2128.84375, 18043.6875), 1000, 0)
+    with jsonlines.open("neutrons_test.jsonl", mode='w', compact=True) as writer:
+        for node in nodes:
+            line = {"id":node.id64,"crds":{"x":node.pos[0],"y":node.pos[1],"z":node.pos[2]}}
+            writer.write(line)
     
 
 def kd_insert(root: Node, data: tuple[float, float, float], id64: int) -> None:
@@ -52,14 +38,14 @@ def kd_insert(root: Node, data: tuple[float, float, float], id64: int) -> None:
     current = root
     while True:
         axis = depth % 3 # 0 = x, 1 = y, 2 = z
-        if data[axis] < current.data[axis]:
+        if data[axis] < current.pos[axis]:
             if current.left == None:
                 current.left = node
                 return
             else:
                 current = current.left
                 depth += 1
-        else: #data[axis] >= current.data[axis]
+        else: #data[axis] >= current.pos[axis]
             if current.right == None:
                 current.right = node
                 return
@@ -67,8 +53,24 @@ def kd_insert(root: Node, data: tuple[float, float, float], id64: int) -> None:
                 current = current.right
                 depth += 1
 
+# https://opendsa-server.cs.vt.edu/ODSA/Books/CS3/html/KDtree.html Example 15.5.3
+def radius_search(kd_root: Node, point: tuple[float, float, float], dist: float, depth: int) -> list[Node]:
+    if kd_root == None:
+        return []
+    node_list = []
+    axis = depth % 3 # 0 = x, 1 = y, 2 = z
+    data = kd_root.pos
+    if node_in_radius(kd_root, point, dist):
+        node_list.append(kd_root)
+    if data[axis] >= (point[axis] - dist):
+        node_list.extend(radius_search(kd_root.left, point, dist, depth + 1))
+    if data[axis] < (point[axis] + dist):
+        node_list.extend(radius_search(kd_root.right, point, dist, depth + 1))
+
+    return node_list
+
 def node_in_radius(node: Node, point: tuple[float, float, float], dist: float) -> bool:
-    data = node.data
+    data = node.pos
     if find_coord_distance(data[0], data[1], data[2], point[0], point[1], point[2]) < dist:
         return True
     return False
--- neutrons_A_star.py
+++ neutrons_A_star.py
@@ -1,19 +1,47 @@
 # A lot of this was taken from https://www.datacamp.com/tutorial/a-star-algorithm
 import heapq
-import neutrons_kdtree
+import kdtree
+import math
 
-class A_Node:
-    def __init__(self, coords: tuple[float, float, float], g: float, h: float, id64: int):
+class A_Node: # terrible since it's nowhere consistent with nodes in kdtree, but I'm too lazy to change it rn
+    def __init__(self, coords: tuple[float, float, float], jumps: int, h: float, id64: int):
         self.coords = coords
-        self.g = g
+        self.jumps = jumps
         self.h = h
-        self.f = h + g
+        self.f = jumps + h
         self.parent = None
+        self.fuel = None
         self.id64 = id64
 
-def heuristic_calc(x1, y1, z1):
+# https://opendsa-server.cs.vt.edu/ODSA/Books/CS3/html/KDtree.html Example 15.5.3
+def radius_search(kd_root: kdtree.Node, point: tuple[float, float, float], dist: float, depth: int) -> list[kdtree.Node]:
+    if kd_root == None:
+        return []
+    node_list = []
+    axis = depth % 3 # 0 = x, 1 = y, 2 = z
+    data = kd_root.pos
+    if node_in_radius(kd_root, point, dist):
+        node_list.append(kd_root)
+    if data[axis] >= (point[axis] - dist):
+        node_list.extend(radius_search(kd_root.left, point, dist, depth + 1))
+    if data[axis] < (point[axis] + dist):
+        node_list.extend(radius_search(kd_root.right, point, dist, depth + 1))
+
+    return node_list
+
+def node_in_radius(node: kdtree.Node, point: tuple[float, float, float], dist: float) -> bool:
+    data = node.pos
+    if find_coord_distance(data[0], data[1], data[2], point[0], point[1], point[2]) < dist:
+        return True
+    return False
+
+def find_coord_distance(x1: float, y1: float, z1: float, x2: float, y2: float, z2: float) -> float:
+    return math.sqrt((x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2)
+
+def heuristic_calc(coords: tuple[float, float, float]):
     "Returns estimated best-case jumps from coords to Colonia"
-    dist = neutrons_kdtree.find_coord_distance(x1, y1, z1, -9530.5, -910.28125, 19808.125)
+    x1, y1, z1 = coords
+    dist = find_coord_distance(x1, y1, z1, -9530.5, -910.28125, 19808.125)
     return dist / 554
 
 def reconstruct_path(goal_node: A_Node):
@@ -25,11 +53,66 @@ def reconstruct_path(goal_node: A_Node):
 
     return path[::-1]
 
+# From EDSY https://github.com/taleden/EDSY/blob/master/edsy.js
+# Stats are manually chosen from my Caspian build
+def getJumpFuelCost(dist: float, fuel: float):
+    maxDist = getJumpDistance(fuel)
+    return (dist / maxDist)**2.5025 * min(fuel, 6.8)
+
+def getJumpDistance(fuel: float):
+    dist = (min(fuel, 6.8) / 0.011)**(1 / 2.5025) * (7528.04 / (1194.07 + fuel)) + 10.5
+    return dist * 6
+
 def find_path():
-    kd_tree = neutrons_kdtree.make_tree()
+    print("Starting")
+    kd_tree = kdtree.make_tree()
+    print("Tree made, starting pathfinding")
+    # 3 Capricorni -210.53125, -186.59375, 342.40625
+    start = A_Node((-4682.65625, -2902.625, 17540.3125), 0, heuristic_calc((-4682.65625, -2902.625, 17540.3125)), 22663776292)
+    goal = (-4909.0625, -2045.21875, 19005.40625)
+    # Colonia -9530.5, -910.28125, 19808.125
 
-    start = A_Node((0, 0, 0), 0, heuristic_calc(0, 0, 0), 10477373803)
+    start.fuel = 304
 
-    open_list = [(start.f, start.coords)]
+    open_list = [(start.f, start.coords, start.fuel)]
     open_dict = {start.coords: start}
     closed_set = set()
+    i = 0
+    while open_list:
+        _, current_pos, current_fuel = heapq.heappop(open_list)
+        current_node = open_dict[current_pos]
+
+        if current_pos == goal:
+            return reconstruct_path(current_node)
+        
+        closed_set.add(current_node.id64)
+        if i % 100 == 0:
+            print(f"Node {i} added")
+        i += 1
+
+        for neighbor_node in radius_search(kd_tree, current_pos, getJumpDistance(current_fuel), 0):
+            if neighbor_node.id64 in closed_set:
+                continue
+            neighbor_pos = neighbor_node.pos
+            x1, y1, z1 = neighbor_pos
+            x2, y2, z2 = current_pos
+            fuel_used = getJumpFuelCost(find_coord_distance(x1, y1, z1, x2, y2, z2), current_fuel) # terrible
+
+            if neighbor_pos not in open_dict:
+                neighbor = A_Node(neighbor_pos, current_node.jumps + 1, heuristic_calc(neighbor_pos), neighbor_node.id64)
+                neighbor.parent = current_node
+                neighbor.fuel = current_fuel - fuel_used
+
+                heapq.heappush(open_list, (neighbor.f, neighbor.coords, neighbor.fuel))
+                open_dict[neighbor_pos] = neighbor
+            elif current_node.jumps + 1 <= open_dict[neighbor_pos].jumps and current_fuel - fuel_used >= open_dict[neighbor_pos].fuel:
+                neighbor = open_dict[neighbor_pos]
+                neighbor.jumps = current_node.jumps + 1
+                neighbor.f = neighbor.jumps + neighbor.h
+                neighbor.fuel = current_fuel - fuel_used
+                neighbor.parent = current_node
+    return []
+
+if __name__ == "__main__":
+    path = find_path()
+    print(path)
